58747bba8d031d686691ffbfb0b10523f8083e60
Your Name <you@example.com>
2021-02-03 18:19:19 +0500

    Enum Values

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,19 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
 enum class CalculatorModes {
     None, Addition, Subtraction, Division, Multiplication
 }
+
 class MainActivity : AppCompatActivity() {
-    private val mode = CalculatorModes.Addition
-       override fun onCreate(savedInstanceState: Bundle?) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // val mode = CalculatorModes.Addition
-        //Below line gives error because val cannot be rassigned but var can be assigned multiple times
-      // mode=CalculatorModes.Division
-        tv_name.text= "Calculator mode is $mode"
-
+        var mode = CalculatorModes.Addition
+        tv_name.text = "Calculator mode is $mode"
 
     }
 
