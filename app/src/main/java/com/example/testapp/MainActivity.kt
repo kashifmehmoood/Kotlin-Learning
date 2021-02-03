a841531fb161cc8d972feca080f4f5525f1a2719
Your Name <you@example.com>
2021-02-03 18:13:51 +0500

    Private Properties and constants

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-
+enum class CalculatorModes {
+    None, Addition, Subtraction, Division, Multiplication
+}
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val mode = CalculatorModes.Addition
+       override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var name = "Xyz"
-        Toast.makeText(this, "My name is $name", Toast.LENGTH_SHORT).show()
+       // val mode = CalculatorModes.Addition
+        //Below line gives error because val cannot be rassigned but var can be assigned multiple times
+      // mode=CalculatorModes.Division
+        tv_name.text= "Calculator mode is $mode"
+
 
     }
 
