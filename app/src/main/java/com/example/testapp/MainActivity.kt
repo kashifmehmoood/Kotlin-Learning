714ff0c557b8a21621f3212f6a20bbc386bd34ed
Your Name <you@example.com>
2021-02-03 17:59:17 +0500

    Variables

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -6,15 +6,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    enum class CalculatorModes {
-        None, Addition, Subtraction, Division, Multiplication
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mode = CalculatorModes.Addition
-        tv_name.text = "Calculator mode is $mode"
+        var name = "Xyz"
+        Toast.makeText(this, "My name is $name", Toast.LENGTH_SHORT).show()
 
     }
 
