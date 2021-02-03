864cd6a3906a11109e00dbae1686d8b9ddc9e4ff
Your Name <you@example.com>
2021-02-03 18:35:59 +0500

    Challenge create two variables

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-
-enum class CalculatorModes {
-    None, Addition, Subtraction, Division, Multiplication
-}
-
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mode = CalculatorModes.Addition
-        tv_name.text = "Calculator mode is $mode"
+        var catName = "Diane"
+        var age = 13
+        tv_name.text = "My cat $catName is $age old"
 
     }
 
