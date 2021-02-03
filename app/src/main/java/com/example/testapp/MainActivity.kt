12d9e79235b1e77faa3fc67dac453fa111c28ed9
Your Name <you@example.com>
2021-02-03 17:40:27 +0500

    Initial Commit

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -6,13 +6,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //You can use this variable in whole class and can change value any time
-    var name = "Test"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        name = "Some one else"
-        Toast.makeText(this, "My name is $name", Toast.LENGTH_SHORT).show()
+       var name = "Some one else"
+       tv_name.text="$name"
 
     }
 
