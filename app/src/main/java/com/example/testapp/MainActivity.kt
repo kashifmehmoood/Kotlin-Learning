a015ab38f27aec615ec66c61aed5d4c92367757e
Your Name <you@example.com>
2021-02-03 17:31:35 +0500

    Using Properties

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -6,15 +6,15 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //You can use this variable in whole class and can change value any time
+    var name = "Test"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // You can change variable values
-        var name = "Xyz"
         name = "Some one else"
-        var age = 27
-        Toast.makeText(this, "My name is $name and age is $age", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "My name is $name", Toast.LENGTH_SHORT).show()
 
     }
+
 
 }
