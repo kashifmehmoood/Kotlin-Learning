b4e80f6bc8be2352e08cb3eee7b6c0900214d55b
= <you@example.com>
2021-02-04 16:52:32 +0500

    Boolean values

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,29 +5,21 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isOn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Conditional Statements
+        //Conditional Statements boolean value
         btn_compare.setOnClickListener {
-            if (ed_firstvalue.text.toString().isEmpty()) {
-                ed_firstvalue.setError("Enter First Value..!")
-            } else if (ed_secondvalue.text.toString().isEmpty()) {
-                ed_secondvalue.setError("Enter Second Value..!")
+            if (isOn) {
+                isOn = false
+                tv_value.text = "On"
             } else {
-                compareValue(
-                    Integer.parseInt(ed_firstvalue.text.toString()),
-                    Integer.parseInt(ed_secondvalue.text.toString())
-                )
+                isOn = true
+                tv_value.text = "Off"
             }
-        }
-    }
 
-    private fun compareValue(num1: Int, num2: Int) {
-        if (num1 > num2)
-            tv_value.text = "$num1 is greater than $num2"
-        else
-            tv_value.text = "$num2 is greater than $num1"
+        }
     }
 
 }
