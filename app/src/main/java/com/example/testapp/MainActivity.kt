23e94f882a8dd06692d6cbeeb446a101737f6721
= <you@example.com>
2021-02-04 17:42:55 +0500

    Loops

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,20 +5,16 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var isOn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Using Arrays
+        //Using For Loops
         val buttons = arrayOf(btn_one, btn_two, btn_three)
-        buttons[0].setOnClickListener {
-            pressedButton(1)
-        }
-        buttons[1].setOnClickListener {
-            pressedButton(2)
-        }
-        buttons[2].setOnClickListener {
-            pressedButton(3)
+        for(i in buttons.indices)
+        {
+            buttons[i].setOnClickListener {
+                pressedButton(i+1)
+            }
         }
     }
 
