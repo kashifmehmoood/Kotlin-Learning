4816d7a815ea21895df5ff0fafe7041dd4de902c
Your Name <you@example.com>
2021-02-04 10:27:55 +0500

    Methods

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var catName = "Diane"
-        var age = 13
-        tv_name.text = "My cat $catName is $age old"
+        test()
 
+    }
+
+    fun test() {
+        tv_name.text = "Testing"
     }
 
 
