0f2ebefcb33f883b323fd2540affd9a5d79ceb57
= <you@example.com>
2021-02-04 11:47:07 +0500

    Parameter Methods

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -11,16 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Simple methods without parametersg
-        addNumbers()
+        //Methods with parameters pass integer values
+        addNumbers(15, 20)
+        displayName("Xyz")
 
     }
 
-    fun addNumbers() {
-        var num1 = 12
-        var num2 = 15
+    fun addNumbers(num1: Int, num2: Int) {
         var sum = num1 + num2
         tv_name.text = "The sum of two numbers are $sum"
+    }
+
+    fun displayName(s: String) {
+        Toast.makeText(this, "My name is $s .", Toast.LENGTH_SHORT).show()
     }
 
 
