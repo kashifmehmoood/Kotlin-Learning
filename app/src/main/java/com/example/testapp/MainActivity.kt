453928790de73f50c5cbb017aabb7b1ebe498e3a
= <you@example.com>
2021-02-04 16:16:41 +0500

    Conditional Statments

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -8,21 +8,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Connect button to a methods
-        btn_one.setOnClickListener {
-            displayText("One")
+        //Conditional Statements
+        btn_compare.setOnClickListener {
+            if (ed_firstvalue.text.toString().isEmpty()) {
+                ed_firstvalue.setError("Enter First Value..!")
+            } else if (ed_secondvalue.text.toString().isEmpty()) {
+                ed_secondvalue.setError("Enter Second Value..!")
+            } else {
+                compareValue(
+                    Integer.parseInt(ed_firstvalue.text.toString()),
+                    Integer.parseInt(ed_secondvalue.text.toString())
+                )
+            }
         }
-        btn_two.setOnClickListener {
-            displayText("Two")
-        }
-        btn_three.setOnClickListener {
-            displayText("Three")
-        }
-
     }
 
-    fun displayText(s: String) {
-        tv_name.text = "You pressed button $s"
+    private fun compareValue(num1: Int, num2: Int) {
+        if (num1 > num2)
+            tv_value.text = "$num1 is greater than $num2"
+        else
+            tv_value.text = "$num2 is greater than $num1"
     }
 
 }
